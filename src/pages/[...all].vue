fed7a6a2f2d8fe14b6bd9fa8eef2de79b1f45bc3
felkira <fajarmaulana_math@students.unnes.ac.id>
2023-11-16 15:23:29 +0700

    style: add static assets and color customization

--- src/pages/[...all].vue
+++ src/pages/[...all].vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-    <div>
-        not found
-    </div>
+    <!-- <div style="padding: 60px; display: flex; align-items: center; flex-wrap: wrap; gap: 16px;">
+        <button style=""></button>
+    </div> -->
 </template>
